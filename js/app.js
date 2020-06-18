9f55f87f4d70c2a57d3cf7d602b85303d3feb4a4
Raisul Islam <43632232+Raisul999@users.noreply.github.com>
2020-06-18 13:54:21 +0600

    bug fix

--- js/app.js
+++ js/app.js
@@ -111,7 +111,7 @@ class UI {
         let total = 0;
         if (this.itemList.length > 0) {
             total = this.itemList.reduce(function(acc, curr) {
-                acc = curr.amount;
+                acc += curr.amount;
                 return acc;
 
             }, 0);
